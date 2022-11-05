68e9a0b1441d3630b983fdd017ca3ac38d119a31
Nedu2022 <nwabuokeichinedu00@gmail.com>
2022-11-05 13:36:40 +0100

    mobile section

--- app.js
+++ app.js
@@ -3,3 +3,47 @@ function scrollHeader(){
   if(this.scrollY >= 50) header.classList.add('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader);
+
+
+let swiper = new Swiper(".popular__container", {
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  
+
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+});
+
+const accordionItems = document.querySelectorAll('.value__accordion-item')
+
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector('.value__accordion-header')
+  
+
+  accordionHeader.addEventListener('click', () =>{
+    const openItem = document.querySelector('.accordion-open')
+
+      toggleItem(item)
+  
+      if(openItem && openItem!== item) {
+        toggleItem(openItem)
+      }
+    })
+
+})
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector('.value__accordion-content')
+
+  if(item.classList.contains('accordion-open')) {
+    accordionContent.removeAttribute('style')
+    item.classList.remove('accordion-open')
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + 'px'
+    item.classList.add('accordion-open')
+  }
+
+}
